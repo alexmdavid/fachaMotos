fd1a72c37e369d7a873beb4b0f4ba6b0121962ff
alexdavid <joelarga2020@gmail.com>
2025-08-10 22:49:31 -0500

    ...

--- Program.cs
+++ Program.cs
@@ -50,7 +50,6 @@ builder.Services.AddAuthentication(options =>
 });
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
